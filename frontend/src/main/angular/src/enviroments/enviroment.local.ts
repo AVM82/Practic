a6a6a7684936258b6c17e50a7600b6e416ce28fb
Oleksandr Talavas <o.talavas@gmail.com>
2023-08-02 23:44:35 +0300

    Init commit

---
+++ frontend/src/main/angular/src/enviroments/enviroment.local.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  appApi:'http://localhost:5000'
+};
---
+++ practic/src/main/java/com/group/practic/PracticApplication.java
@@ -0,0 +1,11 @@
+package com.group.practic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(PracticApplication.class, args);
+  }
+}
