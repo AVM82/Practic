39bfca159af2ade9795189dbcd2545cd2510ef04
Oleksandr Talavas <o.talavas@gmail.com>
2023-08-12 15:34:29 +0300

    Add static content filter, change application.properties

--- practic/src/main/java/com/group/practic/CoursesInitializator.java
+++ practic/src/main/java/com/group/practic/CoursesInitializator.java
@@ -12,10 +12,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-
-@Component
 public class CoursesInitializator {
 
     public static final String COURSE_MASK = ".course";
