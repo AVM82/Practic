75e4d49e699e1beee9d833f12f6179cf81a3faeb
kyshin <kyshin1993@gmail.com>
2023-09-20 13:23:15 +0200

    backend: TimeSlotEntity.java - constraints were added StudentReportEntity.java - field likedPersonsIdList was added with getter and setter StudentReportDto.java - fields id and likedPersonsIdList were added with getters and setters StudentReportService.java - method for changing list with persons id who liked the report was added StudentOnCourseController.java  @PutMapping for changing report entity, actually to changing likes list was added

--- practic/src/main/java/com/group/practic/entity/StudentReportEntity.java
+++ practic/src/main/java/com/group/practic/entity/StudentReportEntity.java
@@ -13,6 +13,8 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -38,6 +40,9 @@ public class StudentReportEntity {
 
     @Enumerated(EnumType.STRING)
     ReportState state = ReportState.ANNOUNCED;
+
+
+    List<Long> likedPersonsIdList = new ArrayList<>();
 
 
     public StudentReportEntity() {
@@ -116,5 +121,13 @@ public class StudentReportEntity {
 
     public void setTimeSlot(TimeSlotEntity timeSlot) {
         this.timeSlot = timeSlot;
+    }
+
+    public List<Long> getLikedPersonsIdList() {
+        return likedPersonsIdList;
+    }
+
+    public void setLikedPersonsIdList(List<Long> likedPersonsIdList) {
+        this.likedPersonsIdList = likedPersonsIdList;
     }
 }
