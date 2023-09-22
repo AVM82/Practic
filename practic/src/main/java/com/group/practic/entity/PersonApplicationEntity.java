37c5c50f098294fe40434a3bfbae265bf593e759
Slava <slava-snake@ukr.net>
2023-09-22 21:37:46 +0300

    checkstyle

--- practic/src/main/java/com/group/practic/entity/PersonApplicationEntity.java
+++ practic/src/main/java/com/group/practic/entity/PersonApplicationEntity.java
@@ -1,11 +1,11 @@
 package com.group.practic.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Column;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
