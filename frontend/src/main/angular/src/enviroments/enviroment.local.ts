b197b0bc3b7be5b8f3eacf96bb655e4751b64e97
Inna Burlaka <inna_burlaka@ukr.net>
2023-12-15 21:42:34 +0100

    merge from LightWay

--- frontend/src/main/angular/src/enviroments/enviroment.local.ts
+++ frontend/src/main/angular/src/enviroments/enviroment.local.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   appApi:'',
   apiBaseUrl: "http://localhost:5000/",
-  redirectUrl: "?redirect_uri=http://localhost:5001/login",
+  redirectUrl: "?redirect_uri=http://localhost:5000/login",
   loginBaseUrl: "http://localhost:5000/login",
   logoutBaseUrl: "http://localhost:5000/"
 };
