4e497a7e1f33e558f8889a58ec30162d564a4f8a
Артем Польщак <artem.polschak@gmail.com>
2023-12-23 22:19:51 +0200

    PRAC-107

--- frontend/src/main/angular/src/enviroments/enviroment.prod.ts
+++ frontend/src/main/angular/src/enviroments/enviroment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   appApi:'/api',
-  apiBaseUrl: "http://shpp-practic.eu-north-1.elasticbeanstalk.com/",
-  redirectUrl: "?redirect_uri=http://shpp-practic.eu-north-1.elasticbeanstalk.com/login",
-  loginBaseUrl: "http://shpp-practic.eu-north-1.elasticbeanstalk.com/login",
-  logoutBaseUrl: "http://shpp-practic.eu-north-1.elasticbeanstalk.com/"
+  apiBaseUrl: "http://practically-test.eu-north-1.elasticbeanstalk.com/",
+  redirectUrl: "?redirect_uri=http://practically-test.eu-north-1.elasticbeanstalk.com/login",
+  loginBaseUrl: "http://practically-test.eu-north-1.elasticbeanstalk.com/login",
+  logoutBaseUrl: "http://practically-test.eu-north-1.elasticbeanstalk.com/"
 };
