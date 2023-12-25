428bb0188d9627303a9ed4695274d2a0c4b185e5
Артем Польщак <artem.polschak@gmail.com>
2023-12-25 21:09:35 +0200

    PRAC-107 setup enviroments on front for correct redirection between prodaction and development branches 4

--- frontend/src/main/angular/src/enviroments/enviroment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  production: true,
-  appApi:'/api',
-  apiBaseUrl: "http://shpp-practic.eu-north-1.elasticbeanstalk.com/",
-  redirectUrl: "?redirect_uri=http://shpp-practic.eu-north-1.elasticbeanstalk.com/login",
-  loginBaseUrl: "http://shpp-practic.eu-north-1.elasticbeanstalk.com/login",
-  logoutBaseUrl: "http://shpp-practic.eu-north-1.elasticbeanstalk.com/"
-};
